cc9be8e852548ac7f40c374bd92299c545fee379
Oleg <starostinoleg370@gmail.com>
2018-11-29 18:13:56 +0200

    Add telegram bot

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+const conf = require('dotenv').config();
 
 const app = express();
 const port = 3000;
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_TOKEN;
+const chatId = process.env.CHAT_ID;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//   // send a message to the chat acknowledging receipt of their message
+//   bot.sendMessage(chatId, 'Received your message');
+// });
+
 app.get('/', (request, response) => {
+  bot.sendMessage(chatId, 'Hi from server');
   response.send('Hello from Express!');
 });
 
